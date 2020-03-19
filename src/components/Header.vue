9f6e0384d8fb170b7567dd3d48b916236f57da2c
Janos Mezo <jmezo.asd@gmail.com>
2020-03-19 12:46:38 +0100

    removed css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -109,17 +109,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.header {
-    width: 100%;
-    height: 50px;
-    background: rgb(37, 54, 85)
-}
-.header h1 {
-    color: rgb(0, 185, 255);
-    font-size: 22px;
-    text-align: left;
-    padding: 9px;
-}
+<style>
+
 </style>
