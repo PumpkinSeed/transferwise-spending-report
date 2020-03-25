c3b89f4d4311ca3fff968cd85dae754dbefde7e3
Janos Mezo <jmezo.asd@gmail.com>
2020-03-25 10:23:24 +0100

    added tabs

--- src/components/spending-report/SpendingReport.vue
+++ src/components/spending-report/SpendingReport.vue
@@ -1,29 +1,45 @@
 <template>
-  <div>
+  <div class="d-flex justify-center">
+
     <div v-if="!isBalanceSelected">
       <h1>pick one of your balances to see spending</h1>
-
     </div>
 
     <div v-else>
-      <app-time-range-pick></app-time-range-pick>  
-
-      <div class="columns is-centered">
-        <div class="column is-half">
-          <app-donut-chart v-if="!!categories"></app-donut-chart>
-          <app-spending-amount/>
-          <app-spending-category-talbe v-if="!!categories" :categories="categories"/>
-        </div>
+      <div>
+        <app-time-range-pick></app-time-range-pick>
       </div>
 
+      <!-- <div>
+        <app-spending-amount/>
+      </div> -->
+
+      <div>
+        <v-tabs v-model="tab">
+          <v-tab>pie chart</v-tab>
+          <v-tab>category table</v-tab>
+        </v-tabs>   
+      </div>
+
+      <div>
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <app-donut-chart v-if="!!categories"></app-donut-chart>
+          </v-tab-item>
+          <v-tab-item>
+            <app-spending-category-talbe v-if="!!categories" :categories="categories"/>
+          </v-tab-item>
+        </v-tabs-items>
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
-import SpendingAmount from "./SpendingAmount.vue";
+// import SpendingAmount from "./SpendingAmount.vue";
 import SpendingCategoryTalbe from './SpendingCategoryTable.vue';
 import DonutChart from './DonutChart.vue';
 import TimeRangePick from './TimeRangePick.vue';
@@ -31,7 +47,7 @@ import TimeRangePick from './TimeRangePick.vue';
 export default {
 
   components: {
-    appSpendingAmount: SpendingAmount,
+    // appSpendingAmount: SpendingAmount,
     appSpendingCategoryTalbe: SpendingCategoryTalbe,
     appDonutChart: DonutChart,
     appTimeRangePick: TimeRangePick
@@ -47,6 +63,12 @@ export default {
       return !!this.selectedBalanceCurrency;
     },
   },
+
+  data() {
+    return {
+      tab: null
+    }
+  }
 
 }
 </script>
